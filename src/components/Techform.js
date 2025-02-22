95e86a14fcc34e2771e37ee007cf31724b7fae11
Vinay7789 <vk1428212@gmail.com>
2025-02-22 22:02:13 +0530

    message

--- src/components/Techform.js
+++ src/components/Techform.js
@@ -30,9 +30,9 @@ export default function Techform(props) {
        setText(event.target.value);
     }
     const HandleCopy=()=>{
-      var text1=document.getElementById("mybox");
-      text1.select();
-      navigator.clipboard.writeText(text1.value);
+      
+      navigator.clipboard.writeText(text)
+      props.showAlert("copy text","success")
 
     }
   
@@ -53,10 +53,10 @@ export default function Techform(props) {
   <button disabled={text.length===0} className="btn btn-primary my-3 mx-3" onClick={HandleCopy}>Copy </button>
 < div className="container my-5" style={{color:props.mode==='dark'?'white':"#042743"}}>
 <h1>Your text Summary</h1>
-<p>{text.split(" ").filter((element)=>{
+<p>{text.split(/\s+/).filter((element)=>{
   return element.length!=0}).length} words  {text.length}</p>
 <p>{0.008*text.split(" ").filter((element)=>{
-  return element.length!=0}).length }Minutes read</p>
+  return element.length!==0}).length }Minutes read</p>
 <h2>Preview</h2>
 <p>{text}</p>
 </div>
